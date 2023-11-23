0a05bc373483293d18afdfabaf8c91d8922ba990
DiegoPS00 <diegoperez12salas34@gmail.com>
2023-11-23 17:06:03 -0600

    Agregamos codigo php

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
+$servidor='localhost';
+$cuenta='root';
+$password='';
+$bd='bdprueba1';
+ 
+$conexion = new mysqli($servidor,$cuenta,$password,$bd);
 
+if ($conexion->connect_errno){
+     die('Error en la conexion');
+}
+
+else{
+
+     if(isset($_POST['submit'])&& !empty($_POST['id'])){
+            //obtenemos datos del formulario
+            $id = $_POST['id'];
+            $nom =$_POST['nombre'];
+            $cuenta =$_POST['cuenta'];
+            $pass =$_POST['contra'];
+            
+            $sql = "INSERT INTO usuarios (id, nombre, cuenta, contrasena) VALUES('$id','$nom','$cuenta','$pass')";
+            $conexion->query($sql);  
+            if ($conexion->affected_rows >= 1){ 
+                echo '<script> alert("registro insertado") </script>';
+            }//fin
+         
+         
+     $sql = 'select * from usuarios';
+     $resultado = $conexion -> query($sql); //aplicamos sentencia
+
+     
+    
+     }//fin 
+
+    
+   
+}
 ?>
 
 <!DOCTYPE html>
