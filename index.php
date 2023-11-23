f19e3a84449d3ec3331cc6427ac5b2642373acfe
Checo03 <Valdivia_sergio@outlook.com>
2023-11-22 21:18:43 -0600

    Subir Archivos

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,34 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Prueba Git</h1>
+    <!-- Parte Ricardo -->
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='post'>
+                    <h2>Registro de Usuarios</h2>
+                    <div class="form-group">
+                        <label for="id">ID</label>
+                        <input type="number" name="id" class="form-control" id="id" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="id">Nombre</label>
+                        <input type="text" class="form-control" name="nombre" id="nombre" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="cuenta">Cuenta</label>
+                        <input type="text" id="cuenta" name="cuenta" class="form-control" placeholder=" ">
+                    </div>
+                    <div class="form-group">
+                        <label for="contra">Contrasena</label>
+                        <input name="contra" type="text" class="form-control" id="contra" placeholder="">
+                    </div>
+                    <button class="btn btn-success" type="submit" name="submit">Submit</button>
+                </form>
+            </div> <!-- fin col -->
+        </div> <!-- fin row -->
+    </div> <!-- fin container -->
+    <br><br>
+    <!-- FIN PARTE DE RICARDO -->
 </body>
 </html>
